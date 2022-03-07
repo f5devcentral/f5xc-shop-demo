ae49dde294bfbecc514f74148bad57ec30ce8ee4
Kevin Reynolds <k.reynolds@f5.com>
2022-03-07 00:40:41 -0500

    huh?

--- module/f5xc/main.tf
+++ module/f5xc/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    volterra = {
-      source = "volterraedge/volterra"
-      version = "0.11.3"
-    }
-  }
-}
-
-provider "volterra" {
-  api_p12_file = var.api_p12_file
-  url          = var.api_url
-}
-
 resource "volterra_namespace" "ns" {
   name = var.base
 }
