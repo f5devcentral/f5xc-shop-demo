00946596ba51527d22eab7346ca82ec171cd2a98
Kevin Reynolds <k.reynolds@f5.com>
2022-09-28 15:08:10 -0400

    update provider version

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     volterra = {
       source = "volterraedge/volterra"
-      version = "0.11.3"
+      version = "0.11.13"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
